06a96e0c2debb4ab7f386ea581ae8744d731272b
hitode909 <hitode909@gmail.com>
2010-02-14 20:30:43 +0900

    create exec method

--- src/foo/WebObserver.java
+++ src/foo/WebObserver.java
@@ -7,30 +7,34 @@ import java.io.InputStreamReader;
 
 public class WebObserver implements Runnable {
 
+	private String exec(String command) {
+		String result = "";
+		try {
+			Process process = Runtime.getRuntime().exec(command);
+		    InputStream is = process.getInputStream();
+		    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		    	result += line;
+		    }
+			process.waitFor();
+			return result;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 	@Override
 	public void run() {
 		while(true) {
-		    String command = "/Users/fkd/co/gist/292611/eval_in_firefox 3";
-		    Process process;
-			try {
-				process = Runtime.getRuntime().exec(command);
-			    InputStream is = process.getInputStream();
-			    BufferedReader br = new BufferedReader(new InputStreamReader(is));
-			    String line;
-			    while ((line = br.readLine()) != null) {
-			    	System.out.println(line);
-			    }
-				process.waitFor();
-				process = Runtime.getRuntime().exec("sleep 2");
-				process.waitFor();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		    String got = this.exec("/Users/fkd/co/gist/292611/eval_in_firefox 3");
+		    System.out.println(got);
+		    this.exec("sleep 1");
 		}
 	}
-
 }

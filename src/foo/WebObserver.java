ad3d895e74d3004e7019b781b29dbe3490a8655c
hitode909 <hitode909@gmail.com>
2010-02-16 15:05:22 +0900

    exec and some methods.

--- src/foo/WebObserver.java
+++ src/foo/WebObserver.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.CoreException;
 
 public class WebObserver implements Runnable {
 
@@ -29,12 +31,52 @@ public class WebObserver implements Runnable {
 		return null;
 	}
 	
+	private String exec_with_retry(String command, int retry) {
+		String result;
+		for(int i=0; i<retry; i++) {
+			result = this.exec(command);
+			if (result != null && !result.isEmpty()) {
+				return result;
+			}
+		}
+		return "";
+	};
+	
+	private boolean hasProject(String name) {
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject project = root.getProject(name	);
+		return project.exists();
+	}
+	
+	private IProject getOrCreateProject(String name) {
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject project = root.getProject(name	);
+		if (!project.exists()) {
+		  try {
+		    project.create(null);
+		  } catch (CoreException e1) {
+		    e1.printStackTrace();
+		  }
+		}
+		return project;
+	}
+	
 	@Override
 	public void run() {
 		while(true) {
-		    String got = this.exec("/Users/fkd/co/gist/292611/eval_in_firefox 3");
+		    String got = this.exec_with_retry("/Users/fkd/co/gist/292611/eval_in_firefox 3", 3);
 		    System.out.println(got);
+		    this.dispatch(got);
 		    this.exec("sleep 1");
+		}
+	}
+
+	private void dispatch(String got) {
+		// TODO Auto-generated method stub
+		if (got == "download") {
+			
+		} else if (got == "upload") {
+
 		}
 	}
 }

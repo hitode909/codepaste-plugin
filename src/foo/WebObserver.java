19faa73eb595490fa7bc197848ef71d5b59e2928
hitode909 <hitode909@gmail.com>
2010-02-18 14:29:58 +0900

    timing

--- src/foo/WebObserver.java
+++ src/foo/WebObserver.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -21,6 +22,7 @@ public class WebObserver implements Runnable {
 	}
 	
 	private String eval_with_retry(String arg, int retry) {
+		System.out.println(arg);
 		return this.exec_with_retry(this.command() + " " + arg, retry);
 	}
 
@@ -68,25 +70,19 @@ public class WebObserver implements Runnable {
 		return null;
 	}
 	
-	private IProject createProject(String name, Integer suffix) {
+	private IProject getOrCreateProject(String name) {
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IProject project = root.getProject(suffix == null ? name : name + "-" + suffix);
+		IProject project = root.getProject(name);
 		if (!project.exists()) {
-		  try {
-		    project.create(null);
-		  } catch (CoreException e1) {
-		    e1.printStackTrace();
-		    return null;
-		  }
-		  return project;
-		} else {
-			return this.createProject(name, suffix == null ? 1 : suffix + 1);
+		    try {
+				project.create(null);
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
 		}
-	}
-	
-	private IProject createProject(String projectName) {
-		// TODO Auto-generated method stub
-		return this.createProject(projectName, null);
+		return project;
 	}
 
 
@@ -108,11 +104,11 @@ public class WebObserver implements Runnable {
 
 	private void upload() {
 		// TODO Auto-generated method stub
+		this.eval_with_retry("content.wrappedJSObject.start()", 3);
 		final String projectName = this.eval_with_retry("(content.document.querySelector('meta[name=\"project\"]') || {}).content", 3);
 		final String fileName = this.eval_with_retry("(content.document.querySelector('meta[name=\"file\"]') || {}).content", 3);
-		if (projectName.isEmpty() || fileName.isEmpty()) return;
-		this.eval_with_retry("content.wrappedJSObject.start()", 3);
 		try {
+			if (projectName.isEmpty() || fileName.isEmpty()) throw new Exception("Page seems broken.");
 			IFile file = this.getFileInProject(projectName, fileName);
 			if (file == null) throw new RuntimeException(projectName + "/" + fileName + " not found.");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(file.getContents()));
@@ -124,15 +120,23 @@ public class WebObserver implements Runnable {
 			reader.close();
 			String content = buffer.toString();
 			String encoded_content = URLEncoder.encode(content, "UTF-8");
-			System.out.println(content);
-			System.out.println(encoded_content);
 			this.eval_with_retry("content.wrappedJSObject.setBody('" + encoded_content + "')", 3);
 			this.eval_with_retry("content.wrappedJSObject.succeed()", 3);
 		} catch (Exception e) {
-			this.eval_with_retry("content.wrappedJSObject.failed('" + e.getClass().toString() + ": " + e.getLocalizedMessage() + "')", 3);
+			this.reportError(e);
 		}
 		return;
 		
+	}
+	
+	private void reportError(Exception e) {
+		String message;
+		try {
+			message = URLEncoder.encode(e.getMessage(), "UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			message = e.getMessage();
+		}
+		this.eval_with_retry("content.wrappedJSObject.failed('" + message + "')", 3);
 	}
 	
 	private String parseDisposition(String disposition) {
@@ -145,23 +149,29 @@ public class WebObserver implements Runnable {
 	}
 	
 	private void download() {
+		this.eval_with_retry("content.wrappedJSObject.start()", 3);
 		String href = this.eval_with_retry(
 				"(content.document.querySelector('link[rel=\"download\"]') || {}).href", 3);
 		final String projectName = this.eval_with_retry("(content.document.querySelector('meta[name=\"project\"]') || {}).content", 3);
-		if (href.isEmpty() || projectName.isEmpty()) return;
-		this.eval_with_retry("content.wrappedJSObject.start()", 3);
+		final Boolean allowOverwrite = (this.eval_with_retry("(content.document.querySelector('input#overwrite') || {}).checked", 3)).equals("true");;
 		try {
+			if (href.isEmpty() || projectName.isEmpty()) throw new Exception("Page seems broken.");
 			final URL url = new URL(href);
 			final URLConnection connection = url.openConnection();
 			final String fileName = this.parseDisposition(
 					connection.getHeaderField("Content-Disposition"));
-			IProject project = this.createProject(projectName); //TODO projectName
+			IProject project = this.getOrCreateProject(projectName);
+			if (project == null) throw new RuntimeException("Failed to get project.");
 			project.open(null);
 			IFile file = project.getFile(fileName);
-			file.create(url.openStream(),true, null);
+			if (file.exists() && allowOverwrite) {
+				file.setContents(url.openStream(),IResource.FORCE, null);
+			} else {
+				file.create(url.openStream(),true, null);
+			}
 			this.eval_with_retry("content.wrappedJSObject.succeed('" + project.getName() + "')", 3);
 		} catch (Exception e) {
-			this.eval_with_retry("content.wrappedJSObject.failed('" + e.getMessage() + "')", 3);
+			this.reportError(e);
 		}
 		return;
    }

6207f1bd1b2fd63ac5f90845569f74295b971f10
hitode909 <hitode909@gmail.com>
2010-02-18 16:36:44 +0900

    no message

--- src/foo/StartUp.java
+++ src/foo/StartUp.java
@@ -10,7 +10,6 @@ public class StartUp implements IStartup {
 	@Override
 	public void earlyStartup() {
 		if (this.observer == null) {
-			System.out.println("start");
 			this.observer = new WebObserver();
 			this.thread = new Thread(this.observer);
 			this.thread.start();

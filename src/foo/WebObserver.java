806cea18035a6a53d92af469f8349e4c64bc96cc
hitode909 <hitode909@gmail.com>
2010-02-18 19:23:24 +0900

    it doesn't work...(cannot test)

--- src/foo/WebObserver.java
+++ src/foo/WebObserver.java
@@ -1,18 +1,30 @@
 package foo;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+//import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+//import org.eclipse.jdt.ui.wizards.NewJavaProjectWizardPageTwo;
+import org.eclipse.jdt.launching.IVMInstall;
+import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jdt.launching.LibraryLocation;
 
 public class WebObserver implements Runnable {
 
@@ -70,19 +82,56 @@ public class WebObserver implements Runnable {
 		return null;
 	}
 	
-	private IProject getOrCreateProject(String name) {
+	private IJavaProject getOrCreateProject(String name) {
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 		IProject project = root.getProject(name);
+		IJavaProject javaProject = null;
 		if (!project.exists()) {
 		    try {
 				project.create(null);
+				project.open(null);
+				IProjectDescription description = project.getDescription();
+				String[] natures = description.getNatureIds();
+				String[] newNatures = new String[natures.length + 1];
+				System.arraycopy(natures, 0, newNatures, 0, natures.length);
+				newNatures[natures.length] = JavaCore.NATURE_ID;
+				description.setNatureIds(newNatures);
+				project.setDescription(description, null);
+
+				javaProject = JavaCore.create(project);
+
+				Set<IClasspathEntry> entries = new HashSet<IClasspathEntry>();
+				entries.addAll(Arrays.asList(javaProject.getRawClasspath()));
+
+				String srcFolderName = "src";
+				//ソースフォルダを作成
+				IPath sourcePath = javaProject.getPath().append(srcFolderName);
+				IFolder sourceDir = project.getFolder(new Path(srcFolderName));
+				if (!sourceDir.exists()) {
+					sourceDir.create(false, true, null);
+				}
+				String binFolderName = "bin";
+				// 出力先フォルダを作成
+				IPath outputPath = javaProject.getPath().append(binFolderName);
+				IFolder outputDir = project.getFolder(new Path(binFolderName));
+				if (!outputDir.exists()) {
+					outputDir.create(false, true, null);
+				}
+				// ソースフォルダ、出力フォルダを設定
+				IClasspathEntry srcEntry = JavaCore.newSourceEntry(sourcePath,
+						new IPath[] {javaProject.getPath()}, outputPath);
+				entries.add(srcEntry);
+
+				entries.add(JavaRuntime.getDefaultJREContainerEntry());
+				javaProject.setRawClasspath(entries.toArray(new IClasspathEntry[entries.size()]), null);
+				
 			} catch (CoreException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 				return null;
 			}
 		}
-		return project;
+		return javaProject;
 	}
 
 
@@ -153,23 +202,23 @@ public class WebObserver implements Runnable {
 		String href = this.eval_with_retry(
 				"(content.document.querySelector('link[rel=\"download\"]') || {}).href", 3);
 		final String projectName = this.eval_with_retry("(content.document.querySelector('meta[name=\"project\"]') || {}).content", 3);
-		final Boolean allowOverwrite = (this.eval_with_retry("(content.document.querySelector('input#overwrite') || {}).checked", 3)).equals("true");;
+		final Boolean allowOverwrite = (this.eval_with_retry("(content.document.querySelector('input#overwrite') || {}).checked", 3)).equals("true");
 		try {
 			if (href.isEmpty() || projectName.isEmpty()) throw new Exception("Page seems broken.");
 			final URL url = new URL(href);
 			final URLConnection connection = url.openConnection();
 			final String fileName = this.parseDisposition(
 					connection.getHeaderField("Content-Disposition"));
-			IProject project = this.getOrCreateProject(projectName);
-			if (project == null) throw new RuntimeException("Failed to get project.");
-			project.open(null);
-			IFile file = project.getFile(fileName);
+			IJavaProject jproject = this.getOrCreateProject(projectName);
+			if (jproject == null) throw new RuntimeException("Failed to get project.");
+			jproject.open(null);
+			IFile file = jproject.getProject().getFile(fileName);
 			if (file.exists() && allowOverwrite) {
 				file.setContents(url.openStream(),IResource.FORCE, null);
 			} else {
 				file.create(url.openStream(),true, null);
 			}
-			this.eval_with_retry("content.wrappedJSObject.succeed('" + project.getName() + "')", 3);
+			this.eval_with_retry("content.wrappedJSObject.succeed('" + jproject.getProject().getName() + "')", 3);
 		} catch (Exception e) {
 			this.reportError(e);
 		}
